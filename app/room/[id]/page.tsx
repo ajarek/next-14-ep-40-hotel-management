dc1f2998f11b92ba3536086de77f3e7b8c25a021
Ajarek <ajarek@poczta.onet.pl>
2024-04-24 00:47:28 +0200

    added icons lucide-react

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -1,6 +1,15 @@
 import Image from 'next/image'
 import apartments from '../../../data/apartments.json'
-
+import {
+  Bed,
+  AirVent,
+  Wifi,
+  SquareParking,
+  Trash2,
+  SprayCan,
+  FireExtinguisher,
+  BellElectric,
+} from 'lucide-react'
 const Room = ({ params }: { params: { id: string } }) => {
   const apartment = apartments.find(
     (apartment) => apartment.id === parseInt(params.id)
@@ -38,21 +47,86 @@ const Room = ({ params }: { params: { id: string } }) => {
           </div>
         </div>
       </div>
-      <div className ='grid grid-cols-3 gap-4 max-lg:grid-cols-1 max-lg:grid-rows-2 mt-4'>
-        <div className='col-start-1 col-end-3 max-lg:col-end-2 flex flex-col gap-8 border-2 border-red-500'>
-          <h1 className='text-3xl font-semibold'>{apartment?.title} ({apartment?.area} m<sup>2</sup>)</h1>
+      <div className='grid grid-cols-3 gap-4 max-lg:grid-cols-1 max-lg:grid-rows-2 mt-4'>
+        <div className='col-start-1 col-end-3 max-lg:col-end-2 flex flex-col gap-8 '>
+          <h1 className='text-3xl font-semibold flex justify-between items-center'>
+            <span>{apartment?.title} ({apartment?.area} m<sup>2</sup>)</span>
+            <span>$ {apartment?.price}</span>
+          </h1>
           <div className='grid grid-cols-4 gap-4'>
-            <div className=' rounded-sm w-full h-36 flex justify-center items-center bg-accent'>{apartment?.amenities[0]}</div>
-            <div className=' rounded-sm w-full h-36 flex justify-center items-center bg-accent'>{apartment?.amenities[1]}</div>
-            <div className=' rounded-sm w-full h-36 flex justify-center items-center bg-accent'>{apartment?.amenities[2]}</div>
-            <div className=' rounded-sm w-full h-36 flex justify-center items-center bg-accent'>{apartment?.amenities[3]}</div>
+            <div className=' rounded-sm w-full h-36 flex flex-col justify-center items-center bg-accent'>
+              <Bed />
+              {apartment?.amenities[0]}
+            </div>
+            <div className=' rounded-sm w-full h-36 flex flex-col  justify-center items-center bg-accent'>
+              <AirVent />
+              {apartment?.amenities[1]}
+            </div>
+            <div className=' rounded-sm w-full h-36 flex flex-col justify-center items-center bg-accent'>
+              <Wifi />
+              {apartment?.amenities[2]}
+            </div>
+            <div className=' rounded-sm w-full h-36 flex flex-col justify-center items-center bg-accent'>
+              <SquareParking />
+              {apartment?.amenities[3]}
+            </div>
           </div>
-          <p>{apartment?.description}</p>
-          <p>{apartment?.price}</p>
+          <div>
+            <h2 className='text-2xl font-semibold'>Description</h2>
+            <p>{apartment?.description}</p>
+          </div>
+          <div>
+            <h2 className='text-2xl font-semibold'>Offered Amenities</h2>
+            <div className='grid grid-cols-2 gap-4'>
+              <p className='flex items-center gap-4'>
+                <Bed />
+                {apartment?.amenities[0]}
+              </p>
+              <p className='flex items-center gap-4'>
+                {' '}
+                <AirVent />
+                {apartment?.amenities[1]}
+              </p>
+              <p className='flex items-center gap-4'>
+                <Wifi />
+                {apartment?.amenities[2]}
+              </p>
+              <p className='flex items-center gap-4'>
+                <SquareParking />
+                {apartment?.amenities[3]}
+              </p>
+            </div>
+          </div>
+          <div>
+            <h2 className='text-2xl font-semibold'>Safety And Hygiene</h2>
+            <div className='grid grid-cols-2 gap-4'>
+              <p className='flex items-center gap-4'>
+                <Trash2 />
+                {apartment?.hygiene[0]}
+              </p>
+              <p className='flex items-center gap-4'>
+                {' '}
+                <SprayCan />
+                {apartment?.hygiene[1]}
+              </p>
+              <p className='flex items-center gap-4'>
+                <FireExtinguisher />
+                {apartment?.hygiene[2]}
+              </p>
+              <p className='flex items-center gap-4'>
+                <BellElectric />
+                {apartment?.hygiene[3]}
+              </p>
+            </div>
+          </div>
+          <div>
+          <h2 className='text-2xl font-semibold'>Customer Reviews</h2>
+          <p>Everything great ⭐⭐⭐⭐⭐</p>
+          <p>Not great ⭐⭐⭐</p>
+          </div>
+         
         </div>
-        <div className='col-start-3 col-end-4 max-lg:col-start-1 max-lg:col-end-2 flex flex-col gap-4 border-2 border-red-500'>
-
-        </div>
+        <div className='col-start-3 col-end-4 max-lg:col-start-1 max-lg:col-end-2 flex flex-col gap-4 border-2 border-red-500'></div>
       </div>
     </main>
   )
