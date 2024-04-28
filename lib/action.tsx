29cb5e45820dae19afcf5028ad718a29f95cc697
Ajarek <ajarek@poczta.onet.pl>
2024-04-29 01:12:43 +0200

    added action addUser

--- lib/action.tsx
+++ lib/action.tsx
@@ -1,6 +1,29 @@
 'use server'
-
+import connectToDb from './connectToDb'
+import { User } from './models'
+import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import bcrypt from 'bcryptjs'
+
+export const addUser = async (formData: User) => {
+  const { username, email, password, img, isAdmin } = formData
+  const hashedPassword = await bcrypt.hash(password, 5)
+  try {
+    connectToDb()
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      img,
+      isAdmin,
+    })
+    await newUser.save()
+    console.log('saved' + newUser)
+    revalidatePath('/')
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 export const searchItem = async (formData: FormData) => {
   const search = formData.get('search')
