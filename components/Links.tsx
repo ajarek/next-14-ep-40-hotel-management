4b765578a435abfcc3c304d93272886abbf8df19
Ajarek <ajarek@poczta.onet.pl>
2024-04-28 22:01:06 +0200

    correct menu

--- components/Links.tsx
+++ components/Links.tsx
@@ -5,23 +5,11 @@ import { Menu, X } from 'lucide-react'
 import { useState } from 'react'
 import { usePathname } from 'next/navigation'
 const Links = () => {
-  const [menu, setMenu] = useState(false)
+  
   const pathname = usePathname()
   return (
-    <>
-      <div
-        className=' lg:hidden cursor-pointer z-50'
-        onClick={() => setMenu(!menu)}
-      >
-        {menu ? (
-          <X
-            size={24}
-            color='red'
-          />
-        ) : (
-          <Menu size={24} />
-        )}
-      </div>
+    
+     
       <div
         className={
           'w-full  flex items-center  justify-between  gap-6    italic'
@@ -65,44 +53,9 @@ const Links = () => {
             </Link>
           </div>
         </div>
-        {menu && (
-          <div
-            className='h-screen w-full  absolute left-0 top-0  flex items-center  justify-between     z-10'
-            onClick={() => setMenu(false)}
-          >
-            <div
-              className=' w-1/2 absolute top-0 left-0  flex flex-col pt-20 pb-8 gap-6 bg-secondary lg:hidden z-40'
-              onClick={() => setMenu(false)}
-            >
-              <Link
-                href='/'
-                className={`flex items-center gap-4 hover:bg-primary hover:text-primary-foreground hover:rounded-sm  rounded-sm px-4 py-1 transition ${pathname === '/' ? 'active ' : 'px-4'
-                }`}
-              >
-                Home
-              </Link>
-
-              <Link
-                href='/rooms'
-                className={`flex items-center gap-4 hover:bg-primary hover:text-primary-foreground hover:rounded-sm  rounded-sm px-4 py-2 transition ${
-                  pathname === '/rooms' ? 'active ' : 'px-4'
-                }`}
-              >
-                Rooms
-              </Link>
-              <Link
-                href='/contact'
-                className={`flex items-center gap-4 hover:bg-primary hover:text-primary-foreground hover:rounded-sm  rounded-sm px-4 py-2 transition ${
-                  pathname === '/contact' ? 'active ' : 'px-4'
-                }`}
-              >
-                Contact
-              </Link>
-            </div>
-          </div>
-        )}
+       
       </div>
-    </>
+    
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { ModeToggle } from '@/components/ModeToggle'
 import Links from './Links'
+import MobileNav from './MobileNav'
 
 const Navbar = async () => {
   return (
-    <>
       <div className='h-16 w-full  flex justify-between items-center gap-4 px-8 '>
         <Links />
         <div className='flex justify-between items-center italic gap-6 ml-6'>
           <ModeToggle />
         </div>
+        <div className='"w-full max-w-[264px] lg:hidden'>
+          <MobileNav />
+        </div>
       </div>
-    </>
+    
   )
 }
 
